2761fb454f3be953771800c7e3e2619c1b8387d2
dharmonjames2025 <Jiwann1017@gmail.com>
2026-08-21 18:04:01 +0800

    Beautiful fonts - Outfit, Plus Jakarta Sans, Inter

--- pages/_document.js
+++ pages/_document.js
@@ -4,21 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Browser Tab Title - RVC ESPORTS */}
-        <title>RVC ESPORTS</title>
-        
-        {/* Favicon */}
-        <link rel="icon" href="/images/RVCLOGO.jpg" type="image/jpeg" />
-        <link rel="apple-touch-icon" href="/images/RVCLOGO.jpg" />
-        
-        {/* Fonts */}
+        {/* Beautiful Google Fonts */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700;800;900&family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&family=Inter:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        
-        <meta name="description" content="RVC Esports" />
+        <meta name="description" content="RVC Esports Management" />
         <meta name="theme-color" content="#0a0a0a" />
+        <link rel="icon" href="/images/RVCLOGO.jpg" type="image/jpeg" />
       </Head>
       <body>
         <Main />
